4696da2ed3aec525819cf91ffe99e40f621234f9
Doi Kento <doi-kento-astro@g.ecc.u-tokyo.ac.jp>
2020-06-06 22:10:34 +0900

    :recycle: refactor

--- gmn/model.py
+++ gmn/model.py
@@ -1,70 +1,83 @@
 import torch.nn as nn
-
-from GMN.affinity_layer import Affinity
-from GMN.power_iteration import PowerIteration
+from gmn.affinity_layer import Affinity
+from gmn.displacement_layer import Displacement
+from gmn.power_iteration import PowerIteration
+from utils.build_graphs import reshape_edge_feature
+from utils.fgm import construct_m
 from utils.sinkhorn import Sinkhorn
 from utils.voting_layer import Voting
-from GMN.displacement_layer import Displacement
-from utils.build_graphs import reshape_edge_feature
-from utils.feature_align import feature_align
-from utils.fgm import construct_m
-
-from utils.config import cfg
-
-import utils.backbone
-CNN = eval('utils.backbone.{}'.format(cfg.BACKBONE))
 
 
-class Net(CNN):
-    def __init__(self):
-        super(Net, self).__init__()
-        self.affinity_layer = Affinity(cfg.GMN.FEATURE_CHANNEL)
-        self.power_iteration = PowerIteration(max_iter=cfg.GMN.PI_ITER_NUM, stop_thresh=cfg.GMN.PI_STOP_THRESH)
-        self.bi_stochastic = Sinkhorn(max_iter=cfg.GMN.BS_ITER_NUM, epsilon=cfg.GMN.BS_EPSILON)
-        self.voting_layer = Voting(alpha=cfg.GMN.VOTING_ALPHA)
+class GMNLayer(nn.Module):
+    def __init__(
+            self, node_feature_dim,
+            pi_max_iter, pi_stop_thresh, sh_max_iter, sh_epsilon, v_alpha,
+            voting=True, bi_stochastic=True):
+        super(GMNLayer, self).__init__()
+
+        # voting and bi_stochastic
+        self.voting = voting
+        self.bi_stochastic = bi_stochastic
+
+        # define layers
+        self.affinity_layer = Affinity(node_feature_dim // 2)
+        self.power_iteration = PowerIteration(max_iter=pi_max_iter, stop_thresh=pi_stop_thresh)
+        self.bi_stochastic_layer = Sinkhorn(max_iter=sh_max_iter, epsilon=sh_epsilon)
+        self.voting_layer = Voting(alpha=v_alpha)
         self.displacement_layer = Displacement()
-        self.l2norm = nn.LocalResponseNorm(cfg.GMN.FEATURE_CHANNEL * 2, alpha=cfg.GMN.FEATURE_CHANNEL * 2, beta=0.5, k=0)
+        self.l2norm = nn.LocalResponseNorm(
+            node_feature_dim,
+            alpha=node_feature_dim, beta=0.5, k=0
+        )
 
-    def forward(self, src, tgt, P_src, P_tgt, G_src, G_tgt, H_src, H_tgt, ns_src, ns_tgt, K_G, K_H, type='img'):
-        if type == 'img' or type == 'image':
-            # extract feature
-            src_node = self.node_layers(src)
-            src_edge = self.edge_layers(src_node)
-            tgt_node = self.node_layers(tgt)
-            tgt_edge = self.edge_layers(tgt_node)
+    def forward(
+            self, U_src, U_tgt,
+            F_src, F_tgt, G_src, G_tgt, H_src, H_tgt, ns_src, ns_tgt, K_G, K_H,
+            P_src=None, P_tgt=None):
+        """
 
-            # feature normalization
-            src_node = self.l2norm(src_node)
-            src_edge = self.l2norm(src_edge)
-            tgt_node = self.l2norm(tgt_node)
-            tgt_edge = self.l2norm(tgt_edge)
+        Args:
+            U_src (torch.Tensor): node features of source graph (shape: [b, node_number, feature_dim])
+            U_tgt (torch.Tensor): node features of target graph (shape: [b, node_number, feature_dim])
+            F_src (torch.Tensor): edge features of source graph (shape: [b, edge_number, feature_dim])
+            F_tgt (torch.Tensor): edge features of target graph (shape: [b, edge_number, feature_dim])
+            G_src (torch.Tensor): node-edge incidence matrix (shape:[b, node_number, edge_number])
+            G_tgt (torch.Tensor): node-edge incidence matrix (shape:[b, node_number, edge_number])
+            H_src (torch.Tensor): node-edge incidence matrix (shape:[b, node_number, edge_number])
+            H_tgt (torch.Tensor): node-edge incidence matrix (shape:[b, node_number, edge_number])
+            ns_src (int): number of source node
+            ns_tgt (int): number of target node
+            K_G (torch.Tensor): kronecker product of G2, G1
+            K_H (torch.Tensor): kronecker product of H2, H1
+            P_src (torch.Tensor, optional): position of source node
+            P_tgt (torch.Tensor, optional): position of target node
 
-            # arrange features
-            U_src = feature_align(src_node, P_src, ns_src, cfg.PAIR.RESCALE)
-            F_src = feature_align(src_edge, P_src, ns_src, cfg.PAIR.RESCALE)
-            U_tgt = feature_align(tgt_node, P_tgt, ns_tgt, cfg.PAIR.RESCALE)
-            F_tgt = feature_align(tgt_edge, P_tgt, ns_tgt, cfg.PAIR.RESCALE)
-        elif type == 'feat' or type == 'feature':
-            U_src = src[:, :src.shape[1] // 2, :]
-            F_src = src[:, src.shape[1] // 2:, :]
-            U_tgt = tgt[:, :tgt.shape[1] // 2, :]
-            F_tgt = tgt[:, tgt.shape[1] // 2:, :]
-        else:
-            raise ValueError('unknown type string {}'.format(type))
+        Returns:
+            s (torch.Tensor): confidence matrix
+            d (torch.Tensor, optional): displacement vector
+        """
 
+        # construct edge features
         X = reshape_edge_feature(F_src, G_src, H_src)
         Y = reshape_edge_feature(F_tgt, G_tgt, H_tgt)
 
-        # affinity layer
+        # construct affinity matrix
         Me, Mp = self.affinity_layer(X, Y, U_src, U_tgt)
-
         M = construct_m(Me, Mp, K_G, K_H)
 
+        # solve assignment problem
         v = self.power_iteration(M)
-        s = v.view(v.shape[0], P_tgt.shape[1], -1).transpose(1, 2)
+        s = v.view(v.size(0), U_tgt.size(1), U_src.size(1)).transpose(1, 2)
 
-        s = self.voting_layer(s, ns_src, ns_tgt)
-        s = self.bi_stochastic(s, ns_src, ns_tgt)
+        # make double-stochastic matrix
+        if self.bi_stochastic:
+            s = self.bi_stochastic_layer(s, ns_src, ns_tgt)
 
-        d, _ = self.displacement_layer(s, P_src, P_tgt)
-        return s, d
+        # convert confidence-maps to displacements
+        if self.voting:
+            s = self.voting_layer(s, ns_src, ns_tgt)
+            if (P_src is not None) and (P_tgt is not None):
+                d, _ = self.displacement_layer(s, P_src, P_tgt)
+            return s, d
+        else:
+            return s
